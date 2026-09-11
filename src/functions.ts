abaa33be310eb479b7bdc5b3dd807569574f0082
Nicholas West <npwest@udel.edu>
2026-09-11 13:23:46 -0400

    finished all tests!

--- src/functions.ts
+++ src/functions.ts
@@ -32,7 +32,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return (message.endsWith("?") ? true : false);
 }
 
 /**
@@ -41,5 +41,5 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    return (word.toLowerCase() === "yes" ? true : (word.toLowerCase() === "no" ? false : null))
 }
